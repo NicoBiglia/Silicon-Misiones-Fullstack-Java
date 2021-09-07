f0b4312028eba574ec842c3ffb37bb9f05b48348
NicoBiglia <82480156+NicoBiglia@users.noreply.github.com>
2021-09-06 23:50:06 -0300

    Mini Calculadora en Java
    Este nuevo proyecto el cual realice con la interfaz grafica Swift de Java, para poner en practica los conocimientos aprendidos el día de hoy me fue muy divertido de hacer, estoy conforme con el resultado, luego iré incorporando mas funcionalidades.

---
+++ Calculadora/src/logica/Calculadora.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author Nicolas Biglia
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+       Pantalla screen = new Pantalla();
+       screen.setVisible(true);
+       screen.setLocationRelativeTo(null);
+    }
+}
